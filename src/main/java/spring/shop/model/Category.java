9679b8172db18eff903699439ec7ec9106d0fdb6
Aigul <aigul.sibgatulli@mail.ru>
2022-02-10 14:32:04 +0300

    init project

---
+++ src/main/java/spring/shop/model/Category.java
@@ -0,0 +1,22 @@
+package spring.shop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/spring/shop/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package spring.shop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import spring.shop.model.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
