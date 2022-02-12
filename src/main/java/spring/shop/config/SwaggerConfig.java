f328fb9ec77ca9685cc9bbf28baceff2d1073df3
Aigul <aigul.sibgatulli@mail.ru>
2022-02-12 18:36:25 +0300

    configure swagger

---
+++ src/main/java/spring/shop/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package spring.shop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    // ui - http://localhost:8080/swagger-ui/index.html
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Shop",
+                "Pet Project",
+                "1.0",
+                "Terms of service",
+                new Contact("me",
+                        "@aiguls_9",
+                        "email@gmail.com"),
+                "License",
+                "license.com",
+                Collections.emptyList());
+    }
+}
--- src/main/java/spring/shop/controller/CustomerController.java
+++ src/main/java/spring/shop/controller/CustomerController.java
@@ -3,7 +3,6 @@ package spring.shop.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import spring.shop.api.v1.dto.CustomerDto;
 import spring.shop.api.v1.mapper.CustomerMapper;
 import spring.shop.model.Customer;
@@ -19,7 +19,7 @@ import spring.shop.service.CustomerService;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @Slf4j
 @RequestMapping("api/v1/customers")
 public class CustomerController {
